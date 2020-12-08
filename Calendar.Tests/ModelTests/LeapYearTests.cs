b4a0241cace56c6121e1e2b63ad37fe8ea333d8f
Jeff Chiu <jwchiu82@gmail.com>
2020-12-07 20:25:46 -0800

    add red fail for years multiple of 100

--- Calendar.Tests/ModelTests/LeapYearTests.cs
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -19,5 +19,12 @@ namespace Calendar.TestTools
             LeapYear testLeapYear = new LeapYear();
             Assert.AreEqual(false, testLeapYear.IsLeapYear(1999));
         }
+
+        [TestMethod]
+        public void IsLeapYear_MultiplesOfOneHundred_False()
+        {
+            LeapYear testLeapYear = new LeapYear();
+            Assert.AreEqual(false, testLeapYear.IsLeapYear(1900));
+        }
     }
 }
