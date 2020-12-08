487f1199593d084315e833c42a8f6654bf620214
Jeff Chiu <jwchiu82@gmail.com>
2020-12-07 20:27:29 -0800

    add green pass for years % 100

--- Calendar/Models/LeapYear.cs
+++ Calendar/Models/LeapYear.cs
@@ -4,7 +4,14 @@ namespace Calendar
     {
         public bool IsLeapYear(int Year)
         {
+            if (Year % 100 == 0)
+            {
+                return false;
+            }
+            else
+            {
             return Year % 4 == 0;
+            }
         }
     }
 }
